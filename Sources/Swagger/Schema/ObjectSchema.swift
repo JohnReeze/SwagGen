b89c88b02af99365acfbd78cfeb94ab66b6857f5
Mikhail Monakov <monakov@surfstudio.co>
2020-01-19 16:36:31 +0300

    add oublic init for Property

--- Sources/Swagger/Schema/ObjectSchema.swift
+++ Sources/Swagger/Schema/ObjectSchema.swift
@@ -33,6 +33,14 @@ public struct Property {
     public let name: String
     public let required: Bool
     public let schema: Schema
+
+    public init(name: String,
+                required: Bool,
+                schema: Schema) {
+        self.name = name
+        self.required = required
+        self.schema = schema
+    }
 }
 
 extension ObjectSchema: JSONObjectConvertible {
